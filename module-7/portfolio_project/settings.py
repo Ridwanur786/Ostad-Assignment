6bb93a26aeeebacbd0d4bf75fe9c70d90b7d7d80
Ridwanur786 <rahmankhan1607@gmail.com>
2026-08-23 23:58:17 +0600

    settings.py Updated

--- module-7/portfolio_project/settings.py
+++ module-7/portfolio_project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfolio_project',       
         'USER': 'postgres',       
-        'PASSWORD': 'Uni#123', 
+        'PASSWORD': '', 
         'HOST': 'localhost',         
         'PORT': '5432',              
     }
